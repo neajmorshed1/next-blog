08b03824ad790d3a80dd067c366eaf403270cb3c
Neaj Morshed Imon <neajmorshedimon@gmail.com>
2022-12-01 21:40:19 +0600

    post card

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Custom404() {
   return (
     <section className="flex h-screen justify-center items-center flex-col">
       <div className="container m-auto px-4">
         <div className="max-w-lg m-auto text-center flex flex-col gap-4">
-          <h2 className="text-3xl font-bold text-gray-700">Page Not Found</h2>
-          <p className="text-base text-gray-500">
+          <h2 className="text-4xl font-bold text-gray-700">Page Not Found</h2>
+          <p className="text-base text-gray-500 font-body">
             The page your are looking for doesn't exist or has been removed.
           </p>
           <div>
---
+++ src/pages/all-post.js
@@ -0,0 +1,280 @@
+import Link from 'next/link';
+import React from 'react';
+import { RiEyeLine, RiChat4Line } from 'react-icons/ri';
+export default function AllPost() {
+  return (
+    <div>
+      <section className="flex justify-center items-center flex-col pt-28 pb-28">
+        <div className="container m-auto px-4 mb-10">
+          <div className="max-w-lg m-auto text-center">
+            <span className="text-sm font-medium font-body text-blue-600 uppercase">
+              6 Results For
+            </span>
+            <h2 className="text-3xl font-extrabold text-gray-800 mb-3">
+              Webflow Interactions
+            </h2>
+            <form action="">
+              <div className="flex gap-3">
+                <input
+                  type="text"
+                  className="border border-gray-300 h-12 px-4 py-3 w-full placeholder:text-gray-400 text-sm"
+                  placeholder="Webflow Interactions"
+                />
+                <button className="bg-blue-700 transition-all  text-sm font-medium text-white px-5 py-3 inline-flex items-center hover:bg-blue-800">
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="container m-auto px-4">
+          <div className="grid lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>{' '}
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>{' '}
+            <article className="border border-gray-300 p-4">
+              <Link href="/">
+                <div>
+                  <img src="/card-1.jpg" alt="" />
+                </div>
+                <div className="px-2 pt-4">
+                  <h3 className="text-gray-800 font-extrabold text-xl hover:text-blue-700">
+                    Add web animation with After Effects & Lottie
+                  </h3>
+                  <p className="text-base text-gray-600 py-3 font-normal font-body">
+                    A small river named Duden flows by their place and supplies
+                    it with the necessary..
+                  </p>
+                </div>
+                <div className="flex gap-x-4 px-2">
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiChat4Line className="w-5 h-5" />
+                    <span className="text-base">3</span>
+                  </div>
+                  <div className="flex items-center gap-2 text-gray-500 font-medium">
+                    <RiEyeLine className="w-5 h-5" />
+                    <span className="text-base">14.2K</span>
+                  </div>
+                </div>
+              </Link>
+            </article>{' '}
+          </div>
+        </div>
+      </section>
+      <footer className="bg-blue-100">
+        <div className="container m-auto px-4">
+          <div className="py-24">hello</div>
+          <div className="flex justify-between border-y border-gray-300 py-16">
+            <div>
+              <h3 className="font-extrabold text-xl text-gray-800">Blog</h3>
+              <nav className="mt-3">
+                <ul className="flex flex-col gap-3 font-semibold text-gray-700 ">
+                  <li>
+                    <Link href="/" className="hover:text-blue-700">
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" className="hover:text-blue-700">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" className="hover:text-blue-700">
+                      Contact
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/" className="hover:text-blue-700">
+                      Article
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
